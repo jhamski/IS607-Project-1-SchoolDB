22ad60661f20a7a046a34ccba587df2e16b40ff4
jhamski <hamski.2@gmail.com>
2015-02-21 12:33:34 -0500

    This it the repo for the first project in IS607

---
+++ Project_1_SchoolDB.sql
@@ -0,0 +1,14 @@
+﻿/*
+IS607 Project 1
+
+Group Members: 
+
+Create a database for a school
+
+1. Describe the purpose of the school (to its customers or community), and of your database (to the school). 
+One paragraph of text will be sufficient. 
+2. Create the tables for the database, populate the table with records, and write statements that report on all of the
+tables.
+
+ */
+
